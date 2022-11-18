0a64928af14dc64b40c873bceb36362d7e59df7d
Bob <bob.yuan@yahooo.com>
2022-11-18 10:13:53 -0800

    Fixed the login request issue: using lombak to create default constructor(both no arg and all arg)

--- src/main/java/com/skyware/jwtauth/dtos/requests/LoginRequest.java
+++ src/main/java/com/skyware/jwtauth/dtos/requests/LoginRequest.java
@@ -2,18 +2,21 @@ package com.skyware.jwtauth.dtos.requests;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Objects;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public final class LoginRequest {
-    private final String username;
-    private final String password;
+    private String username;
+    private String password;
 
-    public LoginRequest(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
+//    public LoginRequest(String username, String password) {
+//        this.username = username;
+//        this.password = password;
+//    }
 
 }
 
